dec2aa61d010abfba208652612e91066189af872
Sean Johnston <c3328440@uon.edu.au>
2019-04-16 10:07:56 +0000

    Test 3

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include <stdio.h> 
 
-int main() {
-    
- printf("Hello");
-    
 
+int main() {
+    int key = 1;
+    char characters[100];
+    int output;
+    
    
     
+    scanf("%c", &characters[100]);
+    
+    for(int i=0; characters[100] != '\0'; i++){
+    output = characters[100]+key%26;
+    printf(" %c ", output);
+   
+    } 
+  
+
+    //printf("Encrypted message: %c", );
+    
+
+    
+ 
     return 0;
 }
