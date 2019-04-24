c2191bc1385e6ea4c01f91b3056cef7001da3ca2
Sean Johnston <c3328440@uon.edu.au>
2019-04-24 08:47:13 +0000

    UPDATE

--- main.c
+++ main.c
@@ -19,19 +19,21 @@ int main() {/*
     }
    */
     int key = 2;
-    char characters[100]= "JGNNQ YQTNF";
+    char characters[]= "VJG SWKEM DTQYP HQZ LWORGF QXGT VJG NCBA FQI";
     int output;
+    
     int i=0;
     
-    for(i=0;characters[i] != '\0'; i++){
-        if(characters[i] == ' '){
-            output = characters[i];
+    for(i=0;characters[i] != '\0'; i++){        
+       if(characters[i] == ' '){
+           output=characters[i];
            printf("%c", output);
         }
-        else 
-        output = (characters[i]-65 - key) % 26 + 65;
+       else 
+        output = (characters[i]-65 - key) % 26 +65 ;
         printf("%c", output);
-    } 
-    
+        
+        
+   }
     return 0;
 }
