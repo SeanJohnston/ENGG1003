474a17be8b3b57ec6499555cb80f4dff6fcaa382
Sean Johnston <c3328440@uon.edu.au>
2019-04-24 07:19:17 +0000

    Fix spacing problem

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h> 
 
-int main() {
+int main() {/*
     int key = 2;
     char characters[100]= "HELLO WORLD";
     int output;
@@ -8,13 +8,30 @@ int main() {
     
     for(i=0;characters[i] != '\0'; i++){
         if(characters[i] == ' '){
-           printf(" ");
+        output = characters[i];
+        printf("%c", output);
+           
+           
         }
         else
         output = (characters[i]-65 + key) % 26 + 65;
         printf("%c", output);
     }
+   */
+    int key = 2;
+    char characters[100]= "JGNNQ YQTNF";
+    int output;
+    int i=0;
     
+    for(i=0;characters[i] != '\0'; i++){
+        if(characters[i] == ' '){
+            output = characters[i];
+           printf("%c", output);
+        }
+        else 
+        output = (characters[i]-65 - key) % 26 + 65;
+        printf("%c", output);
+    } 
     
     return 0;
 }
