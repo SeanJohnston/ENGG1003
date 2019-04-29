b6d34784cb8b40bee2ab09afae21335d93183208
Sean Johnston <c3328440@uon.edu.au>
2019-04-29 04:30:11 +0000

    FINAL PRODUCT

--- main.c
+++ main.c
@@ -9,17 +9,14 @@ int main() {
     switch(menu){
         case 1:{
              int key = 2;
-             char characters[]= "HELLO WORLD";
+             char characters[100];
              int output;
              int i=0;
-             
-             printf("\nYour Encrypted Message Is : ");
-               for(i=0;characters[i] != '\0'; i++){
-                  if(characters[i] == ' '){
-                  output = characters[i];
-                  printf("%c", output);
-                  }
-                  else
+             printf("Enter Message You Wish To Encrypt And Then Your Rotation Key\n");
+             scanf("%s", &characters);
+             scanf("%d", &key);
+             printf("Your Encrypted Message Is : ");
+               for(i=0;characters[i] != '\0'; i++){      
                   output = (characters[i]-65 + key) % 26 + 65;
                   printf("%c", output);
                   }
@@ -27,30 +24,31 @@ int main() {
                break;
 
         case 2:{
-             int key2 = 2;
-             char characters2[]= "VJG SWKEM DTQYP HQZ LWORGF QXGT VJG NCBA FQI";
+             int key2;
+             char characters2[100];
              int output2;
-             
              int i2=0;
-     
+             printf("Enter Message You Wish To Decrypt And Then Your Rotation Key\n");
+             scanf("%s", &characters2);
+             scanf("%d", &key2);
+             printf("Your Decrypted Message Is : ");
               for(i2=0;characters2[i2] != '\0'; i2++){        
-                 if(characters2[i2] == ' '){
-                 output2=characters2[i2];
-                 printf("%c", output2);
-                 }
-                 else
                  output2 =(characters2[i2]-65-key2) % 26 +65;
                  if(output2<65 && output2>39){
                      output2 = output2 +26;}
+                     
                      printf("%c", output2);
                  }
 
                } 
                
                break;
-              }
-
               
+        default: 
+             printf("***ERROR***\nINVALID ENTRY");
+
+          }
+
   
     return 0;
 }
